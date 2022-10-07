dece10aafc305b4ad7e8e691538469f3ffa4da0a
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-07 03:39:17 -0400

    post route - store in db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,4 +9,22 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
+//POST - add to db.json and return to app. 
+app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // create user note object
+    let taskNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 1000)
+    };
+    // pushing taskNote to db.json
+    db.push(taskNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+});
+
+
 }
