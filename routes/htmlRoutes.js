ce90f4a7a1722594ac8589b6340c7797f4e9d581
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-07 03:59:12 -0400

    added routes to starter code html files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+// routing
+module.exports = (app) => {
+
+    // GET /notes and return notes.html
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes/html'));
+    });
+
+// GET wildcard and returns index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+};
