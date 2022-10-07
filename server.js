7ac8cecdda032f63d077be01c569cd6becb46056
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-07 04:03:05 -0400

    added routes requirement in server.js

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+
+// routes to routes files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// lister to start the server
+app.listen(PORT, () => {
+    console.log(`Server available at localhost:${PORT}`);
+});
