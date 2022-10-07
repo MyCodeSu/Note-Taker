6a84b3261dd175c2be9ea7d185140b7465396e07
michaelsu01@gmail.com <michaelsu01@gmail.com>
2022-10-07 03:52:43 -0400

    add delete route targeting ID.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,27 +4,33 @@ const fs = require('fs');
 // routing
 module.exports = (app) => {
 
-// GET
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
+    // GET
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
 
-//POST - add to db.json and return to app. 
-app.post('/api/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
-    // create user note object
-    let taskNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: Math.floor(Math.random() * 1000)
-    };
-    // pushing taskNote to db.json
-    db.push(taskNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
-});
+    //POST - add to db.json and return to app. 
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        // create user note object
+        let taskNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: Math.floor(Math.random() * 1000)
+        };
+        // pushing taskNote to db.json
+        db.push(taskNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
 
-
-}
+    // DELETE route
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let deleteTask = db.filter(item => item.id !== req.params.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteTask));
+        res.json(deleteTask);
+    })
+};
